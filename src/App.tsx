fdbd2216e1a9724ba41e6e01876dac0cdbf265f3
n0xZ <o.gonzalo@hotmail.com>
2022-10-11 17:47:56 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js'
+
+import logo from './logo.svg'
+
+const App: Component = () => {
+	return (
+		<div>
+			<p>Hello world!</p>
+		</div>
+	)
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+import WindiCSS from 'vite-plugin-windicss'
+import Pages from 'vite-plugin-pages'
+export default defineConfig({
+	plugins: [solidPlugin(), WindiCSS(), Pages()],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: 'esnext',
+	},
+})
