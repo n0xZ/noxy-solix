ce629e995b6cc1c2b6e0897c4dc9bbe1b1091f6a
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-10-12 16:35:11 -0300

    setup vite PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import WindiCSS from 'vite-plugin-windicss'
+import { VitePWA } from 'vite-plugin-pwa'
 import Pages from 'vite-plugin-pages'
 export default defineConfig({
-	plugins: [solidPlugin(), WindiCSS(), Pages()],
+	plugins: [solidPlugin(), WindiCSS(), Pages(), VitePWA()],
 	server: {
 		port: 3000,
 	},
