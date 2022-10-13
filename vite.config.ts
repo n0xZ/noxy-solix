208158f2df7c024a8851c09cb75c4a4ef1e29f79
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-10-13 13:38:37 -0300

    remove pwa deps due to issues with solid-trpc

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import WindiCSS from 'vite-plugin-windicss'
-import { VitePWA } from 'vite-plugin-pwa'
+
 import Pages from 'vite-plugin-pages'
+
 export default defineConfig({
-	plugins: [solidPlugin(), WindiCSS(), Pages(), VitePWA()],
+	plugins: [solidPlugin(), WindiCSS(), Pages()],
 	server: {
 		port: 3000,
 	},
