b95fc47f232b4baa72ae8bcd546f7f644ff4e5b8
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-10-17 17:35:44 -0300

    set initial layout on home landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
-
+import { NavLink } from '@solidjs/router'
 
 export default function Home() {
-  return (
-    <div>Hello SOlyx</div>
-  )
+	return (
+		<>
+			<header class="p-5 border-dark-800 border-b-2 ">
+				<nav class="flex flex-row items-center justify-between container mx-auto max-w-5xl font-mulish text-lg">
+					<h1>
+						<NavLink href="/">Solix</NavLink>
+					</h1>
+					<ul class="flex flex-row items-center space-x-3">
+						<li>
+							<NavLink href="/login">Iniciar sesión</NavLink>
+						</li>
+						<li>
+							<NavLink href="/register">Unete ya! </NavLink>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<main class='h-screen'>
+				<section></section>
+				<section></section>
+			</main>
+		</>
+	)
 }
