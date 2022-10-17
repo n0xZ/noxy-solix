256b9db751ca4fcf7b3ee53665961801992c130d
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-10-17 17:33:54 -0300

    setup custom fonts/styles

--- windi.config.ts
+++ windi.config.ts
@@ -1,3 +1,11 @@
 import { defineConfig } from 'windicss/helpers'
 
-export default defineConfig({ plugins: [require('windicss/plugin/forms')] })
+export default defineConfig({
+	plugins: [require('windicss/plugin/forms')],
+	theme: {
+		fontFamily: {
+			mulish: ['Mulish', 'sans-serif'],
+			workSans: ['Work Sans', 'sans-serif'],
+		},
+	},
+})
