81e86a7726d3c213faf05c814c33103b7de42c53
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-10-18 17:08:17 -0300

    set title on login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -57,6 +57,7 @@ export default function Login() {
 					onSubmit={onSubmit}
 					class="  p-20 flex flex-col items-center justify-center items-center w-full   xl:max-w-2xl lg:max-w-lg md:max-w-lg container mx-auto space-y-5 "
 				>
+				<h2 class='text-center xl:text-3xl text-lg font-bold mb-3'>Inicia sesión en Solyx!</h2>
 					<FormField
 						autocomplete="off"
 						label="Correo electrónico"
