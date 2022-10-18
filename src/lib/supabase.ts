f8e60fb6cf8f5e0664fae47f6e3fa14b952fcde9
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-10-18 17:06:59 -0300

    add functions that handle DB requests

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,14 +1,22 @@
 import { createClient } from '@supabase/supabase-js'
-import { Invoice, Item } from '~/types'
+import { ProductList } from '~/types'
 
 export const supabase = createClient(
 	import.meta.env.VITE_SUPABASE_URL!,
 	import.meta.env.VITE_SUPABASE_ANON_KEY!
 )
 
-type DatabaseFields = 'invoice' | 'item'
-type SelectedRow<T = DatabaseFields> = T extends 'invoice' ? Invoice : Item
-
-export const getFromDB = async (field: DatabaseFields) => {
-	return await supabase.from<SelectedRow<typeof field>>(field).select()
+export const getProductsListFromDB = async () => {
+	const loggedUser = await supabase.auth.session()
+	if (!loggedUser || !loggedUser.user) throw new Error('User not found.')
+	return await supabase
+		.from<ProductList>('product-list')
+		.select('*')
+		.eq('user_Id', loggedUser?.user?.id)
+}
+export const getProductItemsByProductListId = async (productListId: string) => {
+	return await supabase
+		.from<ProductList>('product-list')
+		.select('*')
+		.eq('productListId', productListId)
 }
