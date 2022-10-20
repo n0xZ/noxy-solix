d3624ecf6bc53b88d2fd0f42f5ae94b9268d649c
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-10-20 16:46:19 -0300

    update link href on product list  item

--- src/components/product/ProductListItem.tsx
+++ src/components/product/ProductListItem.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function ProductListItem(props: Props) {
 	return (
 		<Link
-			href={`/list/${props.productListItem.productListId}`}
+			href={`/home/list/${props.productListItem.productListId}`}
 			class="h-24 w-72 rounded-md border-2 border-dark-800 flex flex-row flex-wrap justify-center items-center text-center p-2 space-x-2"
 		>
 			<p>{props.productListItem.title}</p>
