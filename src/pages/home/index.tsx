24829eef3dbfb6daecbe7419d5a2dc7c843a21f4
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-10-24 16:45:23 -0300

    set produst list content on home and delete list route content

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,20 +1,61 @@
 import { Title } from '@solidjs/meta'
 import { AuthSession } from '@supabase/supabase-js'
-import { createSignal, onMount } from 'solid-js'
-import { supabase } from '~/lib/supabase'
+
+import { createQuery } from '@tanstack/solid-query'
+import { createSignal, Match, onMount, Switch } from 'solid-js'
+import { CreateProductList } from '~/components/product/CreateProductList'
+import ProductsList from '~/components/product/ProductsList'
+import { getProductsListFromDB, supabase } from '~/lib/supabase'
 
 export default function Home() {
+	const query = createQuery(
+		() => ['ProductListsByUserId'],
+		async () => await getProductsListFromDB()
+	)
 	const [existingSession, setExistingSession] = createSignal<AuthSession | null>(
 		null
 	)
+	const [isOpen, setIsOpen] = createSignal(false)
+	function closeModal() {
+		setIsOpen(false)
+	}
+
+	function openModal() {
+		setIsOpen(true)
+	}
 	onMount(() => setExistingSession(supabase.auth.session()))
 	return (
-		<section>
-		<Title>Solyx - Home</Title>
-			<article>
-				<p>Bienvenid@ {existingSession()?.user?.user_metadata.username}</p>
-			</article>
-			<article></article>
+		<section class="h-full  space-y-5 ">
+			<Title>Solyx - Lista de productos</Title>
+			<aside class="w-full p-5 border-b-2 border-dark-800  container mx-auto flex flex-row items-center justify-end">
+				<button onClick={openModal}>Crear nueva lista</button>
+			</aside>
+			<CreateProductList
+				loggedUser={existingSession()?.user}
+				isOpen={isOpen()}
+				closeModal={closeModal}
+			/>
+			<section class="flex h-full xl:flex-row  flex-col items-center justify-center text-center">
+				<Switch>
+					<Match when={query.isLoading}>
+						<p>Cargando lista de productos...</p>
+					</Match>
+					<Match when={query.isError}>
+						<p>
+							Ha ocurrido un error al cargar tu lista de productos. Por favor, vuelva a
+							intentarlo más tarde.
+						</p>
+					</Match>
+					<Match when={query.isSuccess && query.data.data?.length === 0}>
+						<p>No tienes ninguna lista de tareas creadas por el momento.</p>
+					</Match>
+					<Match
+						when={query.isSuccess && query.data && query.data.data?.length !== 0}
+					>
+						<ProductsList productsList={query.data?.data!} />
+					</Match>
+				</Switch>
+			</section>
 		</section>
 	)
 }
