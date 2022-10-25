6f07046b4d349818b5314e74850f92fe435e0c1f
n0xZ <o.gonzalo@hotmail.com>
2022-10-24 22:57:13 -0300

    setup initial config related to PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,44 @@ import solidPlugin from 'vite-plugin-solid'
 import WindiCSS from 'vite-plugin-windicss'
 
 import Pages from 'vite-plugin-pages'
+import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from 'path'
+
 export default defineConfig({
-	plugins: [solidPlugin(), WindiCSS(), Pages()],
+	plugins: [
+		solidPlugin(),
+		WindiCSS(),
+		Pages(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+			manifest: {
+				name: 'Solyx - App',
+				short_name: 'solyx',
+				description: 'Crea tu lista de productos de manera flexible y rápida!',
+				theme_color: '#ffffff',
+				start_url: '/',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
+			},
+		}),
+	],
 	server: {
 		port: 3000,
 	},
