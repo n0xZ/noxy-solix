26bc56ad6bf2ac4b1861caacb70d9181d94c4b17
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-01 09:19:57 -0300

    chore: add item list skeleton to display when loading items

---
+++ src/components/item/item-list-skeleton.tsx
@@ -0,0 +1,56 @@
+export function ItemListSkeleton() {
+	return (
+		<article class="flex flex-col items-center items-center gap-7 container mx-auto  mt-6 p-2">
+			<aside class="h-40 w-full  flex flex-col justify-between max-w-xl  rounded-md bg-dark-800 flex flex-row  text-center  p-6 ">
+				<div class="flex flex-row items-center  justify-between">
+					<div class="h-4 w-48 animate-pulse rounded-lg bg-dark-500"></div>
+					<div class="h-11 w-11  animate-pulse rounded-full bg-dark-500"></div>
+				</div>
+				<div class="flex flex-row items-center  justify-between">
+					<div class="h-4 w-12 animate-pulse rounded-lg bg-dark-500"></div>
+					<div class="h-4 w-24 animate-pulse rounded-lg bg-dark-500"></div>
+				</div>
+			</aside>
+			<aside class="h-40 w-full  flex flex-col justify-between max-w-xl  rounded-md bg-dark-800 flex flex-row  text-center  p-6 ">
+				<div class="flex flex-row items-center  justify-between">
+					<div class="h-4 w-48 animate-pulse rounded-lg bg-dark-500"></div>
+					<div class="h-11 w-11  animate-pulse rounded-full bg-dark-500"></div>
+				</div>
+				<div class="flex flex-row items-center  justify-between">
+					<div class="h-4 w-12 animate-pulse rounded-lg bg-dark-500"></div>
+					<div class="h-4 w-24 animate-pulse rounded-lg bg-dark-500"></div>
+				</div>
+			</aside>
+			<aside class="h-40 w-full  flex flex-col justify-between max-w-xl  rounded-md bg-dark-800 flex flex-row  text-center  p-6 ">
+				<div class="flex flex-row items-center  justify-between">
+					<div class="h-4 w-48 animate-pulse rounded-lg bg-dark-500"></div>
+					<div class="h-11 w-11  animate-pulse rounded-full bg-dark-500"></div>
+				</div>
+				<div class="flex flex-row items-center  justify-between">
+					<div class="h-4 w-12 animate-pulse rounded-lg bg-dark-500"></div>
+					<div class="h-4 w-24 animate-pulse rounded-lg bg-dark-500"></div>
+				</div>
+			</aside>
+			<aside class="h-40 w-full  flex flex-col justify-between max-w-xl  rounded-md bg-dark-800 flex flex-row  text-center  p-6 ">
+				<div class="flex flex-row items-center  justify-between">
+					<div class="h-4 w-48 animate-pulse rounded-lg bg-dark-500"></div>
+					<div class="h-11 w-11  animate-pulse rounded-full bg-dark-500"></div>
+				</div>
+				<div class="flex flex-row items-center  justify-between">
+					<div class="h-4 w-12 animate-pulse rounded-lg bg-dark-500"></div>
+					<div class="h-4 w-24 animate-pulse rounded-lg bg-dark-500"></div>
+				</div>
+			</aside>
+			<aside class="h-40 w-full  flex flex-col justify-between max-w-xl  rounded-md bg-dark-800 flex flex-row  text-center  p-6 ">
+				<div class="flex flex-row items-center  justify-between">
+					<div class="h-4 w-48 animate-pulse rounded-lg bg-dark-500"></div>
+					<div class="h-11 w-11  animate-pulse rounded-full bg-dark-500"></div>
+				</div>
+				<div class="flex flex-row items-center  justify-between">
+					<div class="h-4 w-12 animate-pulse rounded-lg bg-dark-500"></div>
+					<div class="h-4 w-24 animate-pulse rounded-lg bg-dark-500"></div>
+				</div>
+			</aside>
+		</article>
+	)
+}
