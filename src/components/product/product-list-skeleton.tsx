12a7effbf77a2082d88b53341008b56e4a613259
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-01 09:40:43 -0300

    chore: add product list skeleton

---
+++ src/components/product/product-list-skeleton.tsx
@@ -0,0 +1,9 @@
+export  function ProductListSkeleton() {
+	return (
+		<article class="flex flex-col items-center items-center gap-7 container mx-auto  mt-6">
+			<div class="h-40 w-full   max-w-xl  rounded-md bg-dark-800 flex flex-row  text-center  p-6">
+				<div class="h-4 w-48 animate-pulse rounded-lg bg-dark-500"></div>
+			</div>
+		</article>
+	)
+}
